725a795e1666e129fc882c84cfdef44d50925727
nujhedtozu <junior.tzou@gmail.com>
2018-06-08 17:44:42 -0400

    Add error handling; removed "return privateKey" from loadWallet(); minor bug fixes

--- SmartContracts/contractsJS/smartContracts.js
+++ SmartContracts/contractsJS/smartContracts.js
@@ -9,39 +9,62 @@ web3.setProvider(new web3.providers.HttpProvider("https://rinkeby.infura.io/8Dx9
 
 // This function compiles the solidity file. Similar to truffle compile, but it is done here.
 var compileSolFile = async function(solFilePath, contractName) {
-    let readFile = util.promisify(fs.readFile);
-    let solidityCode = await readFile(solFilePath, "utf8");
-    let input = solidityCode.toString();
-    let output = solc.compile(input, 1);
+    var output;
+    try {
+        let readFile = util.promisify(fs.readFile);
+        let solidityCode = await readFile(solFilePath, "utf8");
+        let input = solidityCode.toString();
+        output = solc.compile(input, 1);
+    }
+    catch (err) {
+        console.log(err);
+    }
+
     let compilation = {}
-    compilation.bc = output.contracts[':' + contractName].bytecode;
-    compilation.abi = JSON.parse(output.contracts[':' + contractName].interface);
+    if (output) {
+        compilation.bc = output.contracts[':' + contractName].bytecode;
+        compilation.abi = JSON.parse(output.contracts[':' + contractName].interface);
+    }
+
     return compilation;
 }
 
 var loadWallet = async function(walletPath, accountAddress, password) {
     web3.eth.accounts.wallet.clear();
-    let keyObject = keythereum.importFromFile(accountAddress, walletPath);
+    let keyObject = keythereum.importFromFile(accountAddress.substring(2), walletPath);
     let privateKey = keythereum.recover(password, keyObject);
     privateKey = "0x" + privateKey.toString("hex");
     web3.eth.accounts.wallet.add(privateKey);
-    return privateKey;
 }
 
 var deployContract = async function(abi, bc, gas, gasPrice, from, constructorArgs) {
-    let contract = new web3.eth.Contract(abi);
-    let contractInstance = await contract.deploy({
-        data: '0x' + bc,
-        arguments: constructorArgs
-    }).send({
-        from: from,
-        gas: gas,
-        gasPrice: gasPrice
-    });
-    contractInstance.options.from = from;
-    contractInstance.options.gasPrice = gasPrice;
-    contractInstance.options.gas = gas;
-    console.log("Contract Address: " + contractInstance.options.address);
+    if (gas <= 0) {
+        console.log("deployContract(): must provide a gas value greater than 0");
+        return undefined;
+    }
+
+    var contractInstance;
+    try {
+        let contract = new web3.eth.Contract(abi);
+        contractInstance = await contract.deploy({
+            data: '0x' + bc,
+            arguments: constructorArgs
+        }).send({
+            from: from,
+            gas: gas,
+            gasPrice: gasPrice
+        });
+    } catch (err) {
+        console.log(err);
+    }
+
+    if (contractInstance) {
+        contractInstance.options.from = from;
+        contractInstance.options.gasPrice = gasPrice;
+        contractInstance.options.gas = gas;
+        console.log("Contract Address: " + contractInstance.options.address);
+    }
+
     return contractInstance;
 }
 
@@ -55,16 +78,28 @@ var loadContract = async function(abi, contractAddress, gas, gasPrice, from) {
 }
 
 var run = async function() {
-    await loadWallet('/home/clankster99/.ethereum/rinkeby/', 'f9f83aaa322ab613db21229be6ca9e2df8a1a149', 'tcupManush');
+    let wallet = await loadWallet('/home/jun/.ethereum', '0xde89471e94ffaaf346090abe1ccd4b448818dcbf', 'tcup').catch(
+        function(error) {
+            console.log(error);
+            throw new Error("failed to load wallet");
+        }
+    );
 
-    let compilation = await compileSolFile("../contracts/KuwaRegistration.sol", "KuwaRegistration");
-    let contractInstance =  await deployContract(compilation.abi, compilation.bc, 4300000, '22000000000', "0xF9F83AaA322aB613Db21229BE6ca9E2dF8a1A149", [1000, "0xf9f83aaa322ab613db21229be6ca9e2df8a1a149"]);
+    let compilation = await compileSolFile("KuwaRegistration.sol", "KuwaRegistration");
+    if (Object.keys(compilation).length === 0) {
+        throw new Error("failed to compile solidity contract");
+    }
+
+    let contractInstance =  await deployContract(compilation.abi, compilation.bc, 4300000, '22000000000', "0xde89471e94ffaaf346090abe1ccd4b448818dcbf", [1000, "clientPubKey"]);
+    if (contractInstance == null) {
+        throw new Error("failed to deploy contract");
+    }
 
     // let contractInstance = await loadContract(compilation.abi, "0x30768510F1A57B12817CDC2a723C8AE21de071b5", 4300000, "22000000000", "0xF9F83AaA322aB613Db21229BE6ca9E2dF8a1A149");
 
-    //await contractInstance.methods.generateChallenge().send();
+    await contractInstance.methods.generateChallenge().send();
     let challenge = await contractInstance.methods.getChallenge().call();
     console.log(challenge);
 }
 
-run();
+run().catch(err => console.log(err));
