a4575d3e91e8b491df85510681e6147b5b71ff75
Carlos Mondragon <mondragon52369@hotmail.com>
2018-06-19 20:48:56 -0400

    Convert Video code to React code. The video does not play yet

--- Client/src/js/Video.js
+++ Client/src/js/Video.js
@@ -4,7 +4,65 @@ import { Button, Container, Row, Col, Form, FormGroup, Label, Input } from 'reac
 export default class Video extends Component {
     constructor(props) {
         super(props);
-        this.takeVideo = takeVideo.bind(this);
+        this.takeVideo = this.takeVideo.bind(this);
+        this.captureError = this.captureError.bind(this);
+        this.captureSuccess = this.captureSuccess.bind(this);
+        this.renderVideo = this.renderVideo.bind(this);
+        this.state = {
+            videoStatus: 'waiting'
+        }
+    }
+
+    takeVideo() {
+        var options = {
+            limit: 1,   // Max number of video clips in a single capture operation
+            duration: 15,    // Max duration of each video clip
+            quality: 1  // 0 means low quality, 1 means high quality
+        };
+    
+        // console.log("You are allowed 15 seconds to record the video");
+        // navigator.notification.alert('You are allowed 15 seconds to record the video', null, 'Alert', 'OK');    //wait for user to click ok and then start recording. Use a promise?
+        navigator.device.capture.captureVideo(this.captureSuccess, this.captureError, options);
+    }
+
+    captureError(err) {
+        this.err = err;
+        this.setState({
+            videoStatus: 'error'
+        })
+    }
+
+    captureSuccess(s) {
+        this.videoPath = s[0].fullPath;
+        this.setState({
+            videoStatus: 'success'
+        });
+    }
+
+    renderVideo() {
+        if (this.state.videoStatus === 'success') {
+            return (
+                <Row className="row-kuwa-reg">
+                    <Col>
+                        <video width="400" height="300" controls>
+                            <source src={this.videoPath.substring(7)} type='video/mp4'/>
+                        </video>
+                        <h5>Video stored at: {this.videoPath.substring(7)}</h5>
+                    </Col>
+                </Row>
+            );
+        }
+        let message = "No video recorded yet";
+        if (this.err) {
+            message = "Failed to record video " + JSON.stringify(this.err);
+        }
+        return (
+            <Row className="row-kuwa-reg">
+                <Col>
+                    <h5>{message}</h5>
+                </Col>
+            </Row>
+        );
     }
 
     render() {
@@ -17,42 +75,8 @@ export default class Video extends Component {
                         <div id="msg"></div>
                     </Col>
                 </Row>
+                {this.renderVideo()}
             </Container>
         );
     }
-}
-
-// document.addEventListener("deviceready", init, false);
-	
-// function init() {
-//     document.querySelector("#takeVideo").addEventListener("touchend", takeVideo, false);
-// }
-
-function takeVideo() {
-    var options = {
-        limit: 1,   // Max number of video clips in a single capture operation
-        duration: 15,    // Max duration of each video clip
-        quality: 1  // 0 means low quality, 1 means high quality
-    };
-
-    console.log("You are allowed 15 seconds to record the video");
-    navigator.notification.alert('You are allowed 15 seconds to record the video', null, 'Alert', 'OK');    //wait for user to click ok and then start recording. Use a promise?
-    navigator.device.capture.captureVideo(captureSuccess, captureError, options);
-}
-
-function captureError(err) {
-    console.log("Failed to record video " + JSON.stringify(err));
-    navigator.notification.alert('Failed to record video: ' + err, null, 'Error', 'OK');
-    document.querySelector('#videoArea').innerHTML = "";
-    document.getElementById('msg').textContent = "No video recorded";
-}
-
-function captureSuccess(s) {
-    console.log("Video successfully recorded");
-    var v = "<video width=\"400\" height=\"300\" controls>";
-        v += "<source src='" + s[0].fullPath + "' type='video/mp4'>";
-        v += "</video>";
-    document.querySelector('#videoArea').innerHTML = v;
-    navigator.notification.confirm('Video successfully recorded', null, 'Success', ['OK']);
-    document.querySelector('#msg').textContent = "Video stored at: " + s[0].fullPath;
 }
