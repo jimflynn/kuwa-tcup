6565d632da2cd2cd4fec68111248597bd74e84f2
hrishikesh <hkashyap@umass.edu>
2018-07-13 11:29:32 -0400

    Faucet reactJS changes

--- Faucet/src/App.js
+++ Faucet/src/App.js
@@ -1,124 +1,37 @@
-/**This is the Kuwa Faucet React application. 
-Author - Hrishikesh Kashyap, Last Updated - 06/22/2018
-*/
-
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './kuwa_logo.png';
 import './App.css';
 
-const Web3 = require('web3');
-let web3 = new Web3();
-
-//setting the provider to the local testrpc network
-web3.setProvider(new Web3.providers.HttpProvider('http://localhost:8545'));
-web3.eth.defaultAccount = web3.eth.accounts[0];
 
 class App extends Component {
   
-  constructor (props) {
-    super (props);
-    const MyContract = window.web3.eth.contract([
-  {
-    "constant": true,
-    "inputs": [],
-    "name": "key3",
-    "outputs": [
-      {
-        "name": "",
-        "type": "string"
-      }
-    ],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "constant": true,
-    "inputs": [],
-    "name": "key2",
-    "outputs": [
-      {
-        "name": "",
-        "type": "string"
-      }
-    ],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "constant": true,
-    "inputs": [],
-    "name": "key1",
-    "outputs": [
-      {
-        "name": "",
-        "type": "string"
-      }
-    ],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "constant": true,
-    "inputs": [],
-    "name": "getValidKuwaID",
-    "outputs": [
-      {
-        "name": "",
-        "type": "string"
-      },
-      {
-        "name": "",
-        "type": "string"
-      },
-      {
-        "name": "",
-        "type": "string"
-      }
-    ],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
+
+  constructor () {
+  super()
+  this.state = {
+    KuwaIDList: ''
   }
-]);
-
-    this.state = {
-      ContractInstance: MyContract.at ('0xc3d75507e3c7f94e3bbbefd8254b175d30ba090d')
-    }
-
-    this.getValidKuwaID = this.getValidKuwaID.bind (this);
-
+  this.handleClick = this.handleClick.bind(this)
   }
 
-  
-  getValidKuwaID () {
-    const { getID } = this.state.ContractInstance;
+  handleClick = () => {
+    console.log('Button is clicked!!');
+    
+    this.setState({KuwaIDList: 'xpub6Cg17gepLkXu8uM1Q8Y5ZRCg2yNcbDHcHrBF955LGtL7Tg9cec9xpCzMzEVs9uRSUvwCsh\nxpub6BiWuCaFQEkfHWniH5385b3Ffc7asL6QzWFF5GmbTZMNHRB6StK699QbtGVcA\nxpub6Cs8CzmVHxWCxQdGZY5VFCjnp5fZNf4TLErmaprJdps8y3wePuV7sNnPq7PUj8vwXaed'})
 
-    getID ((err, secret) => {
-      if (err) console.error ('An error occured::::', err);
-      console.log ('Valid Kuwa IDs', secret);
-    })
-  }
+  };
 
-
-
-  render () {
+  render() {
     return (
-      <div className='App'>
+      <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title"> Kuwa Faucet </h1>
+          <h1 className="App-title">Welcome to the Kuwa Faucet (Edina)</h1>
         </header>
+        
+        <button onClick={this.handleClick}>Get Valid Kuwa IDs</button>
 
-        
-        <br />
-        <br />
-        <button onClick={ this.getValidKuwaID }> Get valid Kuwa IDs </button>
-        <br />
-        <br />
-        
+        <p style={{whiteSpace:"pre"}}>{this.state.KuwaIDList}</p>
       </div>
     );
   }
