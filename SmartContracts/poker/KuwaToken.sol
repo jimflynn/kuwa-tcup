9421df23fa3e3faf765976a292738cd0c3ec26a4
nujhedtozu <junior.tzou@gmail.com>
2018-07-24 11:02:31 -0400

    rename EIP20 to ERC20; modify code to adhere to Solidity conventions

--- SmartContracts/poker/KuwaToken.sol
+++ SmartContracts/poker/KuwaToken.sol
@@ -6,14 +6,14 @@
 // Total supply: 1,000,000.000000000000000000
 // Decimals    : 18
 //
-// Enjoy.
 //
-// (c) BokkyPooBah / Bok Consulting Pty Ltd 2018. The MIT Licence.
+// Originally by: (c) BokkyPooBah / Bok Consulting Pty Ltd 2018. The MIT Licence.
+// Reference: https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20.md
 // Modified by Deh-Jun Tzou
 // ----------------------------------------------------------------------------
 pragma solidity ^0.4.24;
 
-import "./EIP20Interface.sol";
+import "./ERC20Interface.sol";
 import "./SafeMath.sol";
 
 // ----------------------------------------------------------------------------
@@ -61,7 +61,7 @@ contract Owned {
 // ERC20 Token, with the addition of symbol, name and decimals and a
 // fixed supply
 // ----------------------------------------------------------------------------
-contract KuwaToken is EIP20Interface, Owned {
+contract KuwaToken is ERC20Interface, Owned {
     using SafeMath for uint;
 
     string public symbol;
@@ -95,59 +95,59 @@ contract KuwaToken is EIP20Interface, Owned {
 
 
     // ------------------------------------------------------------------------
-    // Get the token balance for account `tokenOwner`
+    // Get the token balance for account `_owner`
     // ------------------------------------------------------------------------
-    function balanceOf(address tokenOwner) public view returns (uint balance) {
-        return balances[tokenOwner];
+    function balanceOf(address _owner) public view returns (uint balance) {
+        return balances[_owner];
     }
 
 
     // ------------------------------------------------------------------------
-    // Transfer the balance from token owner's account to `to` account
+    // Transfer the balance from token owner's account to `_to` account
     // - Owner's account must have sufficient balance to transfer
     // - 0 value transfers are allowed
     // ------------------------------------------------------------------------
-    function transfer(address to, uint tokens) public returns (bool success) {
-        balances[msg.sender] = balances[msg.sender].sub(tokens);
-        balances[to] = balances[to].add(tokens);
-        emit Transfer(msg.sender, to, tokens);
+    function transfer(address _to, uint _value) public returns (bool success) {
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        emit Transfer(msg.sender, _to, _value);
         return true;
     }
 
 
     // ------------------------------------------------------------------------
-    // Token owner can approve for `spender` to transferFrom(...) `tokens`
-    // from the token owner's account
+    // Transfer `_value` tokens from the `_from` account to the `_to` account
+    // 
+    // The calling account must already have sufficient _value approve(...)-d
+    // for spending from the `_from` account and
+    // - `_from` account must have sufficient balance to transfer
+    // - Spender must have sufficient allowance to transfer
+    // - 0 value transfers are allowed
+    // ------------------------------------------------------------------------
+    function transferFrom(address _from, address _to, uint _value) public returns (bool success) {
+        require(_value <= balances[_from]);
+        require(_value <= allowed[_from][msg.sender]);
+        require(_to != address(0));
+
+        balances[_from] = balances[_from].sub(_value);
+        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+
+
+    // ------------------------------------------------------------------------
+    // Token owner can approve for `_spender` to transferFrom(...) `_value` 
+    // tokens from the token owner's account
     //
     // https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20-token-standard.md
     // recommends that there are no checks for the approval double-spend attack
     // as this should be implemented in user interfaces 
     // ------------------------------------------------------------------------
-    function approve(address spender, uint tokens) public returns (bool success) {
-        allowed[msg.sender][spender] = tokens;
-        emit Approval(msg.sender, spender, tokens);
-        return true;
-    }
-
-
-    // ------------------------------------------------------------------------
-    // Transfer `tokens` from the `from` account to the `to` account
-    // 
-    // The calling account must already have sufficient tokens approve(...)-d
-    // for spending from the `from` account and
-    // - From account must have sufficient balance to transfer
-    // - Spender must have sufficient allowance to transfer
-    // - 0 value transfers are allowed
-    // ------------------------------------------------------------------------
-    function transferFrom(address from, address to, uint tokens) public returns (bool success) {
-        require(tokens <= balances[from]);
-        require(tokens <= allowed[from][msg.sender]);
-        require(to != address(0));
-
-        balances[from] = balances[from].sub(tokens);
-        allowed[from][msg.sender] = allowed[from][msg.sender].sub(tokens);
-        balances[to] = balances[to].add(tokens);
-        emit Transfer(from, to, tokens);
+    function approve(address _spender, uint _value) public returns (bool success) {
+        allowed[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
         return true;
     }
 
@@ -156,20 +156,20 @@ contract KuwaToken is EIP20Interface, Owned {
     // Returns the amount of tokens approved by the owner that can be
     // transferred to the spender's account
     // ------------------------------------------------------------------------
-    function allowance(address tokenOwner, address spender) public view returns (uint remaining) {
-        return allowed[tokenOwner][spender];
+    function allowance(address _owner, address _spender) public view returns (uint remaining) {
+        return allowed[_owner][_spender];
     }
 
 
     // ------------------------------------------------------------------------
-    // Token owner can approve for `spender` to transferFrom(...) `tokens`
-    // from the token owner's account. The `spender` contract function
+    // Token owner can approve for `_spender` to transferFrom(...) `_value`
+    // tokens from the token owner's account. The `_spender` contract function
     // `receiveApproval(...)` is then executed
     // ------------------------------------------------------------------------
-    function approveAndCall(address spender, uint tokens, bytes data) public returns (bool success) {
-        allowed[msg.sender][spender] = tokens;
-        emit Approval(msg.sender, spender, tokens);
-        ApproveAndCallFallBack(spender).receiveApproval(msg.sender, tokens, this, data);
+    function approveAndCall(address _spender, uint _value, bytes _data) public returns (bool success) {
+        allowed[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        ApproveAndCallFallBack(_spender).receiveApproval(msg.sender, _value, this, _data);
         return true;
     }
 
@@ -184,8 +184,9 @@ contract KuwaToken is EIP20Interface, Owned {
 
     // ------------------------------------------------------------------------
     // Owner can transfer out any accidentally sent ERC20 tokens
+    // I don't understand this...
     // ------------------------------------------------------------------------
-    function transferAnyERC20Token(address tokenAddress, uint tokens) public onlyOwner returns (bool success) {
-        return ERC20Interface(tokenAddress).transfer(owner, tokens);
+    function transferAnyERC20Token(address _tokenAddress, uint _tokens) public onlyOwner returns (bool success) {
+        return ERC20Interface(_tokenAddress).transfer(owner, _tokens);
     }
 }
