0f369f29c3028d6d5a02e7b4e9d2fe10ec13b60a
hrishikesh <hkashyap@umass.edu>
2018-08-04 16:44:16 -0400

    Kuwa Faucet UI changes

--- Faucet/src/components/valid_kuwa_ids/valid_kuwa_ids.js
+++ Faucet/src/components/valid_kuwa_ids/valid_kuwa_ids.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './valid_kuwa_ids.css';
-import axios from 'axios';
 import Popup from 'reactjs-popup';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
@@ -12,10 +11,12 @@ import {
   InputGroupButtonDropdown,
   InputGroupDropdown,
   Input,
+  InputGroupText,
   Fade,
   Collapse,
   Container,
   Row,
+  Label,
   Col,
   Navbar,
   NavbarToggler,
@@ -29,14 +30,30 @@ import {
   DropdownMenu,
   DropdownItem, Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import loading from '../../loading.gif'
+require('dotenv').config()
+
+const Web3 = require('web3')
+const axios = require('axios')
+const EthereumTx = require('ethereumjs-tx')
+const log = require('ololog').configure({ time: true })
+const ansi = require('ansicolor').nice
+
+
 
 function blockLinks(cell, row){
   return `<a href="https://rinkeby.etherscan.io/address/${cell}" target="_blank">${cell}</a>`;
 }
 
 function amountFormatter(cell,row){
-  return `<div>50</div>`;
+  return `<div>2</div>`;
 }
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+
 
 class KuwaFaucet extends Component {
 
@@ -55,7 +72,9 @@ class KuwaFaucet extends Component {
       dropdownValue: 'Choose currency',
       visible: false,
       amountBox: null,
-      fadeIn: true
+      fadeIn: true,
+      payButtonDisabled: true,
+      defaultAmount: '0.02'
     }
 
     this.toggle = this.toggle.bind(this);
@@ -81,17 +100,26 @@ class KuwaFaucet extends Component {
   toggleDropDown(e) {
     this.setState({
       dropdownOpen: !this.state.dropdownOpen,
-      dropdownValue: e.currentTarget.textContent
+      dropdownValue: e.currentTarget.textContent,
+      payButtonDisabled: false
     });
+
     console.log(e.currentTarget.textContent);
   }
 
   onPayBtnClick(){
     this.setState({
       visible: true
-    });
 
-    console.log( this.state.amountBox, this.state.dropdownValue)
+    });
+    console.log('h12');
+
+    var amt = this.state.amountBox
+    console.log(amt);
+
+    
+
+
 
   }
 
@@ -103,6 +131,8 @@ class KuwaFaucet extends Component {
     this.setState({
       [target.name]: target.value
     });
+
+    
   }
 
 
@@ -111,7 +141,6 @@ class KuwaFaucet extends Component {
      this.interval = setInterval(() => {
         axios.get('/sponsorship_requests/Test')
          .then(res => {
-          console.log(res.data);
           this.setState({sponsorship_requests : res.data.sponsorship_requests});
           this.setState({isLoading : false});
         })}, 1000);
@@ -151,10 +180,14 @@ class KuwaFaucet extends Component {
         
         <Container>
           <Row>  
+          
           <InputGroup>
+          
           <Col>
-          <Input placeholder="Enter amount" maxLength="10" name="amountBox" value={ this.state.amountBox } onChange={ this.handleChange }/>
+          <Input placeholder="Enter Standard Payment Amount" maxLength="10" name="amountBox" value={ this.state.amountBox } onChange={ this.handleChange }/>
           </Col>
+          
+          <Col>
           <InputGroupButtonDropdown addonType="append" isOpen={this.state.dropdownOpen} toggle={this.toggleDropDown}>
             <DropdownToggle caret>
               {this.state.dropdownValue}
@@ -164,25 +197,37 @@ class KuwaFaucet extends Component {
               <DropdownItem divider />
             </DropdownMenu>
           </InputGroupButtonDropdown>
+          </Col>
+
           </InputGroup>
+          </Row>
+          <Row>
+          <Col>
+          <InputGroupAddon>
+          <Label check>
+                <Input type="checkbox" id="checkbox2" defaultChecked />{'Use Standard Payment Amount for All'}
+                
+              </Label>
+          </InputGroupAddon>
+          </Col>
           </Row>
         </Container>
 
 
         <br />
-          <Button id="faucet-payment-button" color="success" size="lg" onClick={this.onPayBtnClick}>Pay</Button>{' '}
+          <Button id="faucet-payment-button" color="success" size="lg" disabled={this.state.payButtonDisabled} onClick={this.onPayBtnClick}>Pay</Button>{' '}
           <Alert color="info" isOpen={this.state.visible} toggle={this.onDismiss} fade={this.state.fadeIn} >
         { this.state.amountBox } KuwaCoins are being sent to each of the valid Kuwa IDs
       </Alert>
           <br />
           <br />
-          <h2> List of valid Kuwa IDs</h2>
-          <BootstrapTable data={this.state.sponsorship_requests} options={this.options} cellEdit={ cellEditProp }>
+          
+          <BootstrapTable data={this.state.sponsorship_requests} options={this.options} cellEdit={ cellEditProp } pagination>
             
               <TableHeaderColumn dataField="timestamp" filter={ { type: 'TextFilter', delay: 200 }} isKey dataSort editable={ false } hidden={ true }> Time </TableHeaderColumn>
-              <TableHeaderColumn dataField="client_address" dataSort editable={ false }> Kuwa ID(Address)</TableHeaderColumn>
+              <TableHeaderColumn dataField="client_address" dataSort editable={ false }> Valid Kuwa IDs (Ethereum Addresses)</TableHeaderColumn>
               <TableHeaderColumn dataField="contract_address" dataSort dataFormat={blockLinks} editable={ false }> Registration Contract Address</TableHeaderColumn>
-              <TableHeaderColumn dataField="amount" dataSort dataFormat={amountFormatter} > Amount </TableHeaderColumn>
+              <TableHeaderColumn dataField="amount" dataSort dataFormat={amountFormatter} > Amount in ({this.state.dropdownValue}) </TableHeaderColumn>
 
         </BootstrapTable>
       </div>
