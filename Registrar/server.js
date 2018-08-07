ad616a6a96bf1022dfd5f8ec197a18d5c498f537
Priyadarshi Rath <priyadarshir@umass.edu>
2018-08-07 17:20:24 -0400

    Changes to watcher logic, creation of "MOE" user on DB

--- Registrar/server.js
+++ Registrar/server.js
@@ -9,12 +9,11 @@ const app = express();
 
 var pool = mysql.createPool({
     connectionLimit : 100,
-    host     : 'localhost',
-    user     : 'root',
-    // password: "sqlpassword",
-    password : String.raw`(-h(3~8u"_ZE{lV%m(2SWze$F-7K<$,ej:2+@=-O\43**|>j6!2~uPmeJko[ASo=`,
-    database : 'alpha_kuwa_registrar_moe',
-    timezone : 'local',
+    host: "localhost",
+    user: "moe",
+    password: "Moe@Alpha123",
+    database: "alpha_kuwa_registrar_moe",
+    timezone : '-04:00',
     dateStrings : true
 });
 
@@ -23,17 +22,11 @@ var credentials = {
     cert: fs.readFileSync('/etc/httpd/conf/ssl.crt/alpha_kuwa_org.pem')
 }
 
-// var credentials = {
-//     key : fs.readFileSync('server.key'),
-//     cert: fs.readFileSync('server.cert')
-// }
-
 app.get('/registration', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
     pool.getConnection((error,connection) => {
         if (error) {
             return res.json({"code" : 100, "status" : "Error in connecting to database"});
-            return;
         }
         // console.log('UI backend has connected to Kuwa database!');
         connection.query("SELECT * FROM registration", (err,rows) => {
@@ -50,12 +43,10 @@ app.get('/registration', (req, res) => {
             }
             else {
                 return res.json({"code" : 100, "status" : "Error in querying database"});
-                return;
             }
         });
         connection.on('error', (err) => {
             return res.json({"code" : 100, "status" : "Error in connecting to database"});
-            return;
         })
     });
 });
