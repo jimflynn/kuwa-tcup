6691e6e1c9eccd539a5513ce32ec337161be0046
dehjuntzou <junior.tzou@gmail.com>
2018-08-08 11:43:03 -0400

    Add additional logic to setRegistrationStatusTo()

--- SmartContracts/contracts/KuwaRegistration.sol
+++ SmartContracts/contracts/KuwaRegistration.sol
@@ -20,7 +20,7 @@ contract KuwaRegistration {
     // For Poker Protocol 
     // ---------------------
     KuwaToken kt;
-    address kuwaTokenContract;
+    address kuwaTokenContract;  //"0x2140eFD7Ba31169c69dfff6CDC66C542f0211825"
     //----------------------
 
 	//constructor
@@ -29,6 +29,7 @@ contract KuwaRegistration {
     constructor (address _clientAddress, address _kuwaTokenContract) public payable {
         clientAddress = _clientAddress;
         sponsorAddress = msg.sender;
+        owner = msg.sender;
         kuwaTokenContract = _kuwaTokenContract;
         kt = KuwaToken(_kuwaTokenContract);
         generateChallenge();
@@ -90,14 +91,15 @@ contract KuwaRegistration {
         bool validInputB = newStatus == "Video Uploaded" || newStatus == "QR Code Scanned";
         bool validInputC = newStatus == "Valid" || newStatus == "Invalid";
         require(validInputA || validInputB || validInputC);
+        require( (newStatus == "Valid" || newStatus == "Invalid")
+                 || !((registrationStatus == "Valid" || registrationStatus == "Invalid")
+                 && (newStatus != "Valid" && newStatus != "Invalid")) );
         registrationStatus = newStatus;
     }
 
-    /* Kills this contract and refunds the balance to the Sponsor */
-    function killContract() public {
-        require(msg.sender == sponsorAddress);
-        selfdestruct(sponsorAddress);
-    }
+/*if newStatus is valid or invalid, update
+if currStatus is valid or invalid and (newStatus is not (valid or invalid)) do not update*/
+
 
     /** ---------------------- Poker Protocol ------------------------- */
     struct Vote {
@@ -109,12 +111,12 @@ contract KuwaRegistration {
         bool isPaid;
     }
 
-    uint timeOfFirstVote = 0;
-    mapping(address => Vote) votes;
+    uint public timeOfFirstVote = 0;
+    mapping(address => Vote) public votes;
     address[] public voters;
     function vote(bytes32 _commit) public returns(bool) {
         require(kt.allowance(sponsorAddress, this) == 1);   // Sponsor must provide ante before voting round for incentive
-        require(timeOfFirstVote == 0 || block.timestamp - timeOfFirstVote <= 3600); // Registrars have one hour to vote after the first vote is cast
+        //require(timeOfFirstVote == 0 || block.timestamp - timeOfFirstVote <= 3600); // Registrars have one hour to vote after the first vote is cast
         require(kt.balanceOf(msg.sender) >= 100001);   // Qualified registrars must possess at least 100,000 Kuwa tokens 
         require(kt.allowance(msg.sender, this) == 1);   // Registrars must provide the required ante to vote
         require(!votes[msg.sender].voted);    // Registrars cannot vote more than once
@@ -129,6 +131,27 @@ contract KuwaRegistration {
         votes[msg.sender] = Vote({commit: _commit, voted: true, vote: 2, salt: 0x0, valid: false, isPaid: false});
         return true;
     }
+    
+    /*unction getVote(address voter) public view returns(bytes32, bool) {
+        return (votes[voter].commit, votes[voter].voted);
+    }*/
+
+    function transferTokens(uint _value) public {
+        kt.transfer(address(this), _value);
+    }
+    
+    function transferFromTokens(uint _value) public {
+        kt.transferFrom(msg.sender, this, _value);
+    }
+    
+    
+    function dummy(uint _value) public returns(address, address, uint) {
+        return (msg.sender, this, _value);
+    }
+    
+    function dummy2() public {
+        kt.dummy();
+    }
 
     function remainingTime() public view returns(uint) {
         return block.timestamp - timeOfFirstVote;
@@ -136,7 +159,7 @@ contract KuwaRegistration {
 
     function reveal(uint _vote, bytes32 _salt) public {
         uint timestamp = block.timestamp;
-        require(timestamp - timeOfFirstVote > 3600 && timestamp - timeOfFirstVote <= 7200);
+        //require(timestamp - timeOfFirstVote > 3600 && timestamp - timeOfFirstVote <= 7200);
         require(votes[msg.sender].voted);
         require(_vote == 0 || _vote == 1);
         
@@ -144,16 +167,23 @@ contract KuwaRegistration {
         votes[msg.sender].salt = _salt;
         votes[msg.sender].valid = keccak256(_vote, _salt) == votes[msg.sender].commit ? true : false;
     }
-
-    uint finalStatus = 3;
-    uint dividend;
-    function decide() public returns(bool) {
+    
+    address public owner;
+    uint public finalStatus = 3;
+    uint public dividend = 0;
+    uint public bp = 0;
+    uint public finalPot;
+    uint public valid = 0;
+    uint public invalid = 0;
+    function decide() public returns(uint) {
         require(msg.sender == sponsorAddress);
-        require(block.timestamp - timeOfFirstVote > 7200);
+        //require(block.timestamp - timeOfFirstVote > 7200);
         
-        uint valid = 0;
-        uint invalid = 0;
-        uint finalPot = kt.balanceOf(this);
+        
+        if (!kt.transferFrom(msg.sender, this, 1))
+            return 0;
+        bp = 1;
+        finalPot = kt.balanceOf(this);
         for (uint i = 0; i < voters.length; i++) {
             Vote storage vote = votes[voters[i]];
             if (vote.valid) {
@@ -165,8 +195,12 @@ contract KuwaRegistration {
                 }
             }
         }
+        bp = 2;
 
-        if (invalid > valid) {
+        if (invalid + valid == 0) {
+            dividend = finalPot;
+        }
+        else if (invalid > valid) {
             finalStatus = 0;
             dividend = finalPot / valid;
             setRegistrationStatusTo("invalid");
@@ -180,11 +214,12 @@ contract KuwaRegistration {
             finalStatus = 2;
             dividend = finalPot / (valid + invalid);
         }
-
-        return true;
+        bp = 3;
+        return 1;
+        //return true;
     }
 
-    function payout() private returns(bool) {
+    function payout() public returns(bool) {
         require(finalStatus != 3);
         require(msg.sender == sponsorAddress);
 
