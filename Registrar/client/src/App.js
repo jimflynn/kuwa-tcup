f932f20c01256f149c9585dd88a7c7ea993f6dc2
Priyadarshi Rath <priyadarshir@umass.edu>
2018-08-22 11:52:59 -0400

    Moved table into a component of its own

--- Registrar/client/src/App.js
+++ Registrar/client/src/App.js
@@ -4,90 +4,29 @@ import './App.css';
 import Navigation from './Navigation.js';
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 import '../node_modules/react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
-import loading from './loading.gif';
+
+import Registrations from './components/Registrations.js';
 
 class App extends Component {
 
-	constructor(props) {
-		super(props);
-		this.state = {
-			isLoading: true,
-			registrations:[],
-		};
-		this.toggle = this.toggle.bind(this);
-	}
-
-	toggle() {
-		this.setState({isLoading: !this.state.isLoading});
-	}
-
-	componentDidMount() {
-		this.interval = setInterval(() => {
-			fetch('/registration')
-				.then(response => response.json())
-				.then(table => this.setState({registrations: table}))
-				.then(console.log('DB =', this.state.registrations))
-				.then(this.setState({isLoading : false}))
-                .then(console.log("isLoading = " + this.state.isLoading))
-		}, 3000);
-	}
-
-	componentWillUnmount() {
-		clearInterval(this.interval);
-	}
-
 	render() {
-		if(this.state.isLoading === false) {
-			return (
-				<div className="App">
-				<Navigation />
-					<header className="App-header">
-						<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
-						<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-						<img src={logo} className="App-logo" alt="logo"/>
-						<h1 className="App-title">Kuwa Registrar Database (Moe)</h1>
-					</header>
-					<BootstrapTable ref='table' data={this.state.registrations}>
-						<TableHeaderColumn width='5%'  dataSort={true} dataAlign='center' dataField='registration_id' isKey >
-						ID
-						</TableHeaderColumn>
-						
-						<TableHeaderColumn width='30%' dataSort={true} dataAlign='center' dataField='client_address'>
-						Client Address
-						</TableHeaderColumn>
-						
-						<TableHeaderColumn width='30%' dataSort={true} dataAlign='center' dataField='contract_address'>
-						Contract Address
-						</TableHeaderColumn>
+        return (
+            <div className="App">
+                <Navigation />
+                <header className="App-header">
+                    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
+                    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">Kuwa Registrar Database (Moe)</h1>
+                </header>
 
-						<TableHeaderColumn width='18%' dataSort={true} dataAlign='center' dataField='timestamp' >
-						Timestamp(EDT)
-						</TableHeaderColumn>
+                <div className="body">
+                    <Registrations/>
+                </div>
 
-						<TableHeaderColumn width='17%' dataSort={true} dataAlign='center' dataField='status' >
-						Status
-						</TableHeaderColumn>
-					</BootstrapTable>
-				</div>
-			);
-        }
-		else {
-			return (
-				<div className="App">
-				<Navigation />
-					<header className="App-header">
-						<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
-						<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-						<img src={logo} className="App-logo" alt="logo"/>
-						<h1 className="App-title">Kuwa Registrar Database (Moe)</h1>
-					</header>
-					<div className="loading">
-						<img className="isLoading" src={loading} alt="Loading Data..." />
-					</div>
-				</div>
-			);
-		}
-	}
+            </div>
+        );
+    }
 }
 
 export default App;
